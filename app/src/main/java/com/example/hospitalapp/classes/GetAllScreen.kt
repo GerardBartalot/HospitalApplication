43caa8b96391ac64680abda426a51779ac536eeb
tarde <nilarroyo43@gmail.com>
2024-12-11 15:40:46 +0100

    Nurses with photo in GetAllScreen

--- app/src/main/java/com/example/hospitalapp/classes/GetAllScreen.kt
+++ app/src/main/java/com/example/hospitalapp/classes/GetAllScreen.kt
@@ -1,5 +1,6 @@
 package com.example.hospitalapp.classes
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -7,20 +8,26 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.hospitalapp.R
 import com.example.hospitalapp.ui.theme.HospitalAppTheme
 
-data class Nurse(val id: Int, val name: String, val username: String)
 
+// Modelo de datos
+data class Nurse(val id: Int, val name: String, val username: String,val profilePic : String)
+
+// Clase para manejar los datos hardcodeados
 class CreateNurses {
     val nurses = listOf(
-        Nurse(1, "Nil Arroyo", "ni95_ar"),
-        Nurse(2, "Gerard Bartalot", "GeryBar"),
-        Nurse(3, "Joan Albert Chias", "Jachias"),
-        Nurse(4, "Sergio Arenas", "sejuma"),
+        Nurse(1, "Nil Arroyo", "ni95_ar","perfil1"),
+        Nurse(2, "Gerard Bartalot", "GeryBar","perfil2"),
+        Nurse(3, "Joan Albert Chias", "Jachias","perfil3"),
+        Nurse(4, "Sergio Arenas", "sejuma","perfil4"),
     )
 }
 
@@ -28,6 +35,7 @@ class CreateNurses {
 fun NurseApp(viewModel: CreateNurses, onBackPressed: () -> Unit) {
     Surface(modifier = Modifier.fillMaxSize()) {
         Column {
+            // Botón colocado en la esquina superior izquierda
             Box(
                 modifier = Modifier
                     .fillMaxWidth()
@@ -39,6 +47,7 @@ fun NurseApp(viewModel: CreateNurses, onBackPressed: () -> Unit) {
                 }
             }
             Spacer(modifier = Modifier.height(16.dp))
+            // Lista de enfermeros
             NurseList(nurses = viewModel.nurses)
         }
     }
@@ -65,16 +74,37 @@ fun NurseCard(nurse: Nurse) {
             .padding(vertical = 8.dp),
         elevation = CardDefaults.cardElevation(4.dp)
     ) {
-        Column(modifier = Modifier.padding(16.dp)) {
-            Text(text = "ID: ${nurse.id}", fontSize = 16.sp, fontWeight = FontWeight.Bold)
-            Text(text = "Name: ${nurse.name}", fontSize = 14.sp)
-            Text(
-                text = "Username: ${nurse.username}",
-                fontSize = 14.sp,
-                fontWeight = FontWeight.Light
-            )
+        Row {
+            GreetingImage(profilePic = nurse.profilePic)
+            Column(modifier = Modifier.padding(16.dp)) {
+                Text(text = "ID: ${nurse.id}", fontSize = 16.sp, fontWeight = FontWeight.Bold)
+                Text(text = "Name: ${nurse.name}", fontSize = 14.sp)
+                Text(
+                    text = "Username: ${nurse.username}",
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.Light
+                )
+
+            }
         }
     }
+}
+@Composable
+fun GreetingImage(profilePic : String){
+    val profileImageMap = mapOf(
+        "perfil1" to R.drawable.perfil1,
+        "perfil2" to R.drawable.perfil2,
+        "perfil3" to R.drawable.perfil3,
+        "perfil4" to R.drawable.perfil4
+    )
+    val imageResource = profileImageMap[profilePic] ?: R.drawable.default_pictures
+    val image = painterResource(imageResource)
+    Image(
+        painter = image,
+        contentDescription = null,
+        modifier = Modifier.size(64.dp).padding(8.dp),
+        contentScale = ContentScale.Crop
+    )
 }
 
 @Preview(showBackground = true)
