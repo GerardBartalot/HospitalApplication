e064ac7cac9c53fb0491059935ef086bee045b2d
jachias21 <pl2023357@365.stucom.com>
2025-01-09 17:07:19 +0100

    dependencias añadidas Retrofit importado

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,9 @@ android {
 
 dependencies {
 
+    implementation (libs.retrofit2.retrofit)
+    implementation(libs.okhttp)
+    implementation (libs.converter.gson)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
